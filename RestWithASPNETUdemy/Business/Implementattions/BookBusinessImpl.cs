a38eda19b0c7a653d73730f0a77dff355f8aa1a6
rafael -HS <rafaelzinhohs@outlook.com>
2018-10-16 14:57:08 -0300

    adding book implementanttion

---
+++ RestWithASPNETUdemy/Business/Implementattions/BookBusinessImpl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Business.Implementattions
+{
+    public class BookBusinessImpl : IBookBusiness
+    {
+        private readonly IBookBusiness _repository;
+        public BookBusinessImpl(IBookBusiness repository)
+        {
+            _repository = repository;
+        }
+
+        public Book Create(Book book)
+        {
+            return _repository.Create(book);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<Book> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Book FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public Book Update(Book book)
+        {
+            return _repository.Update(book);
+        }
+    }
+}
---
+++ RestWithASPNETUdemy/Controllers/BooksController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Business;
+namespace RestWithASPNETUdemy.Controllers
+{
+
+    /* Mapeia as requisições de http://localhost:{porta}/api/persons/v1/
+    Por padrão o ASP.NET Core mapeia todas as classes que extendem Controller
+    pegando a primeira parte do nome da classe em lower case [Person]Controller
+    e expõe como endpoint REST
+    */
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BooksController : Controller
+    {
+        //Declaração do serviço usado
+        private IPersonBusiness _personBusiness;
+
+        /* Injeção de uma instancia de IBooksRepository ao criar
+        uma instancia de PersonController */
+        public BooksController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Book book)
+        {
+            return 0; 
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody]Book book)
+        {
+            return 0;
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
